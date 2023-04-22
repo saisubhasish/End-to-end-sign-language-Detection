60323b5fc63ab9a8c01f508fd72e955d7a9a265b
saisubahasish <saisubhasishrout777@gmail.com>
2023-04-22 14:37:39 +0530

    Added yolo file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.1',
+    author= 'iNeuron',
+    author_email= 'saisubhasishrout777@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
