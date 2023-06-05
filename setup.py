2e71c799771dc8ef2530993e35e1e5acef34180b
saisubhasish <saisubhasishrout777@gmail.com>
2023-06-05 08:35:36 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'signLanguages',
-    version= '0.0.1',
+    version= '0.0.2',
     author= 'SaiSubhasish',
     author_email= 'saisubhasishrout777@gmail.com',
     packages= find_packages(),
